f0bcedf0301fcee473677a197bad4ec08668c4d7
prasath4023 <132539727+prasath4023@users.noreply.github.com>
2024-12-31 12:50:41 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const heroText = document.querySelector(".hero-text");
+    heroText.style.transform = "scale(1)";
+    heroText.style.transition = "transform 1.5s ease-in-out";
+});
